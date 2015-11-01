e4c8606955e629cc5f1e49fb2419d4c1200fa57f
jableader <jacobdunk@gmail.com>
2015-11-01 23:48:27 +1100

    votes bug

--- api/votes.py
+++ api/votes.py
@@ -1,30 +1,31 @@
 from datetime import datetime, timedelta
+import time
 from base64 import b64encode, b64decode
 
-_key = b'ReallySuperSafeKey69'
+_key = bytearray('ReallySuperSafeKey69', 'utf-8')
 _key_len = len(_key)
 
 def encrypt(b):
-    return bytes([(b[i] + _key[i % _key_len]) % 256 for i in range(len(b))])
+    return bytearray([(b[i] + _key[i % _key_len]) % 256 for i in range(len(b))])
 
 def decrypt(b):
-    return bytes([(b[i] - _key[i % _key_len]) % 256 for i in range(len(b))])
+    return bytearray([(b[i] - _key[i % _key_len]) % 256 for i in range(len(b))])
 
 def get_pair(lst):
     query = lst.items.extra(select={'random': 'RANDOM()'}).order_by('random')
     return tuple(query[:2])
 
 def create_nonce(pair):
-    unencrypted = '%d|%d|%d' % (pair[0].pk, pair[1].pk, datetime.utcnow().timestamp())
-    encrypted = encrypt(bytes(unencrypted, 'utf-8'))
+    unencrypted = '%d|%d|%.3f' % (pair[0].pk, pair[1].pk, time.time())
+    encrypted = encrypt(bytearray(unencrypted, 'utf-8'))
     return b64encode(encrypted).decode('utf-8')
 
 
 def check_nonce(nonce, pair):
-    decrypted = decrypt(b64decode(bytes(nonce, 'utf-8'))).decode('utf-8')
+    decrypted = decrypt(bytearray(b64decode(bytearray(nonce, 'utf-8')))).decode('utf-8')
     pk1, pk2, timestamp = decrypted.split('|')
 
-    timestamp = datetime.fromtimestamp(int(timestamp))
+    timestamp = datetime.fromtimestamp(float(timestamp))
     isold = (timestamp + timedelta(seconds=30)) < datetime.utcnow()
 
     expected, actual = [int(pk1), int(pk2)], [item.pk for item in pair]
